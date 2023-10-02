afcec25a0f2b2083b93085535228563b8b1406b9
Froches <michaelfavour418@gmail.com>
2023-10-02 14:26:57 -0700

    Quick sort corrected

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -2,8 +2,8 @@
 
 /**
  * swap - swaps two integers in an array
- * @a: first integer to swap
- * @b: second integer to swap
+ * @i: first integer to swap
+ * @j: second integer to swap
  */
 void swap(int *i, int *j)
 {
